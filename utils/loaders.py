be676873bd8ee4836e22146a7ea614d0a91b0d45
yassirf <yf286@cam.ac.uk>
2021-12-01 14:42:14 +0000

    Update loaders.py

--- utils/loaders.py
+++ utils/loaders.py
@@ -167,7 +167,7 @@ def load_model(args, models, num_classes):
 
     if 'densenet' in args.arch:
         model = models.__dict__[args.arch](args = args)
-    elif 'dag' in args.arch:
+    elif 'daf' in args.arch:
         model = models.__dict__[args.arch](args = args)
 
     else: raise ValueError("==> Model architecture can not be loaded.")
