4d2ca8d27a44a64953ced369cc8cc466bec74633
yassirf <yf286@cam.ac.uk>
2021-12-01 12:23:28 +0000

    autoencs

--- models/cifar/autoencoder.py
+++ models/cifar/autoencoder.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import math
 
 __all__ = [
-
+    'daf_small_bottleneck_cnn_autoenconder'
 ]
 
 
@@ -28,11 +28,18 @@ def make_tcnn_block(inplanes, outplanes, kernel_size=3, stride=1, padding=1):
 
 
 class BottleNeckCNNAutoEnconder(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, planes, num_layers=None, **kwargs):
         super(BottleNeckCNNAutoEnconder, self).__init__()
 
         # Loss function
         self.criterion = nn.MSELoss(reduction='mean')
+
+        # Plane mappings
+        self.planes = planes
+
+        # Number of intermediate blocks
+        if num_layers is None: num_layers = [0, 0, 0, 0, 0, 0]
+        self.num_layers = num_layers
 
         # Create encoder-decoder structure
         self.enc = self._make_encoder()
@@ -42,16 +49,36 @@ class BottleNeckCNNAutoEnconder(nn.Module):
         self.foutput = nn.Tanh()
 
     def _make_encoder(self):
-        enc_block1 = make_cnn_block(3, 12, kernel_size=3, stride=2, padding=1)
-        enc_block2 = make_cnn_block(12, 24, kernel_size=3, stride=2, padding=1)
-        enc_block3 = make_cnn_block(24, 128, kernel_size=3, stride=2, padding=1)
-        return nn.Sequential(enc_block1, enc_block2, enc_block3)
+        layers = []
+        layers.append(make_cnn_block(self.planes[0], self.planes[1], kernel_size=3, stride=2, padding=1))
+        layers.extend([make_cnn_block(self.planes[1], self.planes[1], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[0])])
+
+        layers.append(make_cnn_block(self.planes[1], self.planes[2], kernel_size=3, stride=2, padding=1))
+        layers.extend([make_cnn_block(self.planes[2], self.planes[2], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[1])])
+
+        layers.append(make_cnn_block(self.planes[2], self.planes[3], kernel_size=3, stride=2, padding=1))
+        layers.extend([make_cnn_block(self.planes[3], self.planes[3], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[2])])
+
+        return nn.Sequential(*layers)
 
     def _make_decoder(self):
-        dec_block1 = make_tcnn_block(128, 24, kernel_size=3, stride=2, padding=0)
-        dec_block2 = make_tcnn_block(24, 12, kernel_size=3, stride=2, padding=0)
-        dec_block3 = make_tcnn_block(12, 3, kernel_size=3, stride=2, padding=0)
-        return nn.Sequential(dec_block1, dec_block2, dec_block3)
+        layers = []
+        layers.append(make_tcnn_block(self.planes[3], self.planes[4], kernel_size=3, stride=2, padding=0))
+        layers.extend([make_cnn_block(self.planes[4], self.planes[4], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[3])])
+
+        layers.append(make_tcnn_block(self.planes[4], self.planes[5], kernel_size=3, stride=2, padding=0))
+        layers.extend([make_cnn_block(self.planes[5], self.planes[5], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[4])])
+
+        layers.append(make_tcnn_block(self.planes[5], self.planes[6], kernel_size=3, stride=2, padding=0))
+        layers.extend([make_cnn_block(self.planes[6], self.planes[6], kernel_size=3, stride=1, padding=1)
+                       for _ in range(self.num_layers[5])])
+
+        return nn.Sequential(*layers)
 
     def forward_features(self, x):
         return self.enc(x)
@@ -68,4 +95,79 @@ class BottleNeckCNNAutoEnconder(nn.Module):
 
     def get_loss(self, args, outputs, extra, targets):
         return self.criterion(outputs, extra['input']), {}
+
+
+def daf_small_bottleneck_cnn_autoenconder(**kwargs):
+
+    # Transition of planes between blocks
+    planes = [3, 6, 12, 32, 12, 6, 3]
+
+    return BottleNeckCNNAutoEnconder(
+        planes = planes,
+        **kwargs
+    )
+
+
+def daf_xl_small_bottleneck_cnn_autoenconder(**kwargs):
+    # Transition of planes between blocks
+    planes = [3, 6, 12, 32, 12, 6, 3]
+
+    # Additional intermediate layers
+    num_layers = [1, 1, 1, 1, 1, 1]
+
+    return BottleNeckCNNAutoEnconder(
+        planes=planes,
+        num_layers=num_layers,
+        **kwargs
+    )
+
+
+def daf_medium_bottleneck_cnn_autoenconder(**kwargs):
+
+    # Transition of planes between blocks
+    planes = [3, 12, 24, 64, 24, 12, 3]
+
+    return BottleNeckCNNAutoEnconder(
+        planes=planes,
+        **kwargs
+    )
+
+
+def daf_xl_medium_bottleneck_cnn_autoenconder(**kwargs):
+    # Transition of planes between blocks
+    planes = [3, 12, 24, 64, 24, 12, 3]
+
+    # Additional intermediate layers
+    num_layers = [1, 1, 1, 1, 1, 1]
+
+    return BottleNeckCNNAutoEnconder(
+        planes=planes,
+        num_layers=num_layers,
+        **kwargs
+    )
+
+
+def daf_large_bottleneck_cnn_autoenconder(**kwargs):
+
+    # Transition of planes between blocks
+    planes = [3, 12, 32, 128, 32, 12, 3]
+
+    return BottleNeckCNNAutoEnconder(
+        planes=planes,
+        **kwargs
+    )
+
+
+def daf_xl_large_bottleneck_cnn_autoenconder(**kwargs):
+    # Transition of planes between blocks
+    planes = [3, 12, 32, 128, 32, 12, 3]
+
+    # Additional intermediate layers
+    num_layers = [1, 1, 1, 1, 1, 1]
+
+    return BottleNeckCNNAutoEnconder(
+        planes=planes,
+        num_layers=num_layers,
+        **kwargs
+    )
 
